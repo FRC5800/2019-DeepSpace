65b2b426d9b8d22d4f7241e59dc031e8805bc948
FRC5800 <administrativo@frc5800.com>
2019-02-19 20:45:59 -0300

    [Luc] - PID Values

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -22,7 +22,22 @@ public class OI {
 		//CommandBase.joystick.whenPressed(SubsystemJoystick.d_B, new CommandReset());
 		CommandBase.joystick.whenHeld(SubsystemJoystick.d_LT, new CommandCargoIntake(0.5));
 		CommandBase.joystick.whenHeld(SubsystemJoystick.d_RT, new CommandCargoIntake(-0.5));
-		//CommandBase.joystick.whenPressed(SubsystemJoystick.d_A, new CommandHatchIntakeMotor(0.4));
+		//CommandBase.joystick.whenPressed(SubsystemJoystick.d_A, new CommandHatchIntakeMotor(0.4);
+
+		//PID Buttons
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(1235)
+		.setSequential(new CommandArm(1111))); //Cargo1
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(17500)
+		.setSequential(new CommandArm(1380))); //Cargo2
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(16711)
+		.setSequential(new CommandArm(3461))); //Cargo3
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(3785)
+		.setSequential(new CommandArm(300))); //Hatch1
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(18500)
+		.setSequential(new CommandArm(400))); //Hatch2
+		CommandBase.joystick.whenPressed(SubsystemJoystick.j_Three, new CommandLift(17132)
+		.setSequential(new CommandArm(3474))); //Hatch3
+
 		
 	}
 }
